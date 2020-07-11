fa23f8201223f7810beeca04d4f473ee04d42d71
Ashwin Chalaka <ascha@Ashwins-MacBook-Pro.local>
2020-07-10 17:14:18 -0700

    updated class names and ids, also added first set of pictures to left-imgs next to blog_img

--- master.js
+++ master.js
@@ -17,18 +17,19 @@ var circle = 0;
 
 setInterval(function(){
 	if(circle === 0){
-		document.getElementsByClassName("content_box1")[0].style.backgroundColor = "rgb(186, 57, 51)";
-		// mainContent.style.backgroundColor = "rgb(60, 141, 128)";
-		document.getElementById("blogimg").src = "./media/sect1-red-blog.png";
+		document.getElementsByClassName("left_imgs")[0].innerHTML = "<img src='sect1-red-spoon.png'><img src='sect1-red-spoon.png'><img src='sect1-red-spoon.png'><img src='sect1-red-spoon.png'>";
+		document.getElementById("blog_img").src = "./media/sect1-red-blog.png";
 		circle = 1;
 	}else if(circle === 1){
 		document.getElementsByClassName("content_box1")[0].style.backgroundColor = "rgb(60, 141, 128)";
-		document.getElementById("blogimg").src = "./media/sect1-green-blog.png";
+		document.getElementsByClassName("left_imgs")[0].innerHTML = "";
+		document.getElementById("blog_img").src = "./media/sect1-green-blog.png";
 		circle = 2;
 		// mainContent.style.backgroundColor = "rgb(72, 132, 169)";
 	}else{
 		document.getElementsByClassName("content_box1")[0].style.backgroundColor = "rgb(72, 132, 169)";
-		document.getElementById("blogimg").src = "./media/sect1-blue-blog.png";
+		document.getElementsByClassName("left_imgs")[0].innerHTML = "";
+		document.getElementById("blog_img").src = "./media/sect1-blue-blog.png";
 		circle = 0;
 	}
 }, 3000);
